8b4d69231b525df04649bed469eaad09bfbf0188
Emelie Barrington <emelie.barrington@chasacademy.se>
2022-10-25 23:00:52 +0200

    Added some validation tests

--- script.js
+++ script.js
@@ -1,8 +1,52 @@
 //Spara variabler från DOM
-const userGuess = document.querySelector(".user-guess"); 
+const userGuess = document.getElementById("user-guess");
+const button = document.getElementById("btn");
+const feedback = document.querySelector(".feedback");
+const refreshButton = document.querySelector(".refresh"); 
+const list = document.querySelector(".previous-guesses-list"); 
+button.addEventListener("click", runGame); 
+refreshButton.addEventListener("click", refreshGame); 
 
+const randomNumber = randomInteger(100); 
+// console.log(randomNumber); 
+let guessCounter = 0; 
 
-//Generera random nummer funktion
+function runGame () {
+    const validation = userGuess.value; 
+    if (Number.isNaN(validation) || validation < 0 || validation > 100 || validation === "") {
+        feedback.innerHTML = `Please enter a whole number between 0 and 100.`;
+        feedback.style.padding = '1rem'; 
+    }
+    else {
+        guessCounter++; 
+        if (guessCounter === 5) {
+            const highOrLow = compareGuess(userGuess, randomNumber, true); 
+            if (highOrLow === "You won!") {
+                addGuess(userGuess, highOrLow); 
+            }
+            else {
+                addGuess(userGuess, highOrLow); 
+                feedback.innerHTML = `You've run out of guesses. <br> Press the button to play again!`;
+                feedback.style.padding = '1rem'; 
+                refreshButton.classList.toggle("refresh"); 
+                button.remove(); 
+            }
+            
+        }
+        else if (guessCounter > 5) {
+            feedback.innerHTML = `You've run out of guesses. <br> Press the button to play again!`;
+            feedback.style.padding = '1rem'; 
+        }
+        else {
+            const highOrLow = compareGuess(userGuess, randomNumber); 
+            addGuess(userGuess, highOrLow); 
+        }
+    }
+}
+
+function refreshGame () {
+    location.reload(); 
+}
 
 function randomInteger (max) {
     //max + 1 to include the maximun number as well 
@@ -10,36 +54,47 @@ function randomInteger (max) {
     return num;  
 }
 
-console.log(randomInteger(100)); 
-
-//Jämför användarens gissning med random-numret, returnera true or false - funktion
-
-function compareGuess (userNum, randomNum) {
-    // true = user won, false = user lost
-
-    //TO DO: Jämför om numret var lägre eller högre
+function compareGuess (userNum, randomNum, isLastGuess) {
     let result; 
-    if (userNum === randomNum) {
-        result = true; 
+    const user = Number(userNum.value);
+
+
+    if (user === randomNum) {
+        result = "You won!";
+        feedback.innerHTML = `You won! Congratulations! <br> Press the button to play again.`;
+        feedback.style.padding = '1rem'; 
+        refreshButton.classList.toggle("refresh"); 
+        button.remove();
+    }
+    else if (user > randomNum) {
+        result = "Too high!"; 
+        if (isLastGuess) {
+            feedback.innerHTML = `You guessed ${user} which is too high.`;
+            feedback.style.padding = '1rem'; 
+        }
+        else {
+            feedback.innerHTML = `You guessed ${user} which is too high. <br> Try again...`;
+            feedback.style.padding = '1rem'; 
+        }
     }
     else {
-        result = false; 
+        result = "Too low!";
+        if (isLastGuess) {
+            feedback.innerHTML = `You guessed ${user} which is too low.`;
+            feedback.style.padding = '1rem'; 
+        }
+        else {
+            feedback.innerHTML = `You guessed ${user} which is too low. <br> Try again...`;
+            feedback.style.padding = '1rem'; 
+        }
+        
     }
     return result; 
 }
 
-//Ta emot true or false från jämförelsen, skriv ut resultat - skicka resultat till nästa funktion
 
-function printResult (userWon) {
-    //TO DO: Printa i html
-    if (userWon) {
-        console.log("Congrats! You won!"); 
-    }
-    else {
-        console.log("You lost. Try again!"); 
-    }
+function addGuess (guess, comparison) {
+    list.innerHTML += `<li>You guessed ${guess.value}. ${comparison}</li>`;
 }
 
-//Printa ut tidigare gissningar 
 
-//Funktion som körs när användaren klickar
